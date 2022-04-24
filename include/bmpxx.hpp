18ecb8b6ffcd022e1b9862ae20c26e7b967fcda0
rubikscraft <contact@rubikscraft.nl>
2022-04-24 22:58:40 +0200

    solve bitorder bug

--- include/bmpxx.hpp
+++ include/bmpxx.hpp
@@ -77,9 +77,9 @@ namespace bmpxx
 
     struct RgbaColor
     {
-      uint8_t red;
-      uint8_t green;
       uint8_t blue;
+      uint8_t green;
+      uint8_t red;
       uint8_t alpha;
     };
 
